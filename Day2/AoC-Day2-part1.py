ece2fd405fa7d2868830382d6c921d806e559769
Pete T <pete@blueseabluesky.com>
2021-12-12 14:19:05 +0000

    Updates to the reduce function

--- Day2/AoC-Day2-part1.py
+++ Day2/AoC-Day2-part1.py
@@ -6,16 +6,34 @@ print(sys.version)
 class Vector:
     def __init__(self, movement, amount):
         self.movement = movement
-        self.amount = amount
+        self.amount = (int)(amount)
+        self.summer = 0
     def __str__(self) -> str:
         return self.movement
     def horizontal(self):
         return (self.amount if self.movement == "forward" else 0)
-
-
-
+    def vertical(self):
+        if self.movement == "down" :
+            return self.amount
+        elif self.movement == "up" :
+            return -self.amount
+        else :
+            return 0
         
 
+
+def movement_str(move):
+    return Vector(move.split(" ")[0],move.split(" ")[1])
+
+def horizontalCount(move1, move2):
+    move1.amount = move1.amount + move2.amount
+    return move1
+
+        
+test = movement_str("up 9")
+
+print(test.horizontal())
+print(test.vertical())
 
 f = open("/Users/petertindale/Source/AoC-python/Day2/list.txt", "r")
 
@@ -23,13 +41,15 @@ move_list = f.readlines()
 
 f.close()
 
-print(move_list)
+#print(move_list)
 
-subVector = tuple[str, int]
 
-move_list = list(map(lambda x : Vector(x.split(" ")[0],x.split(" ")[1]), move_list))
+move_list = list(map(movement_str, move_list))
 
-hor = fn.reduce((lambda x, y : x.horizontal()), move_list)
+hor = fn.reduce(horizontalCount, move_list)
 
-print(move_list[0].movement)
-print(move_list[1].amount)
+print(hor.amount)
+
+
+#print(move_list[0].movement)
+#print(move_list[1].amount)
