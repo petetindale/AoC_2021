793152d3fb1b5af98bc620d7cefbf4a688c471ed
Pete T <pete@blueseabluesky.com>
2021-12-13 11:29:50 +0000

    Updating to bring in central function and imports

---
+++ Day3/Day3_Part1.py
@@ -0,0 +1,23 @@
+def get_binary_diagnostic(list_of_strings):
+    list_of_binaries = list(map(lambda x : list(map(int, x.strip())), list_of_strings))
+
+    #length_of_binary = list_of_binaries[0].
+
+    print(list_of_binaries)
+    return 0
+
+
+#test
+get_binary_diagnostic(
+    ["00100\n",
+    "11110\n",
+    "10110\n",
+    "10111\n",
+    "10101\n",
+    "01111\n",
+    "00111\n",
+    "11100\n",
+    "10000\n",
+    "11001\n",
+    "00010\n",
+    "01010\n"])
