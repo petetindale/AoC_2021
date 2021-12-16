27d97b3ed9c9d7421e9bc2576e862b1a8fbebc05
Pete T <pete@blueseabluesky.com>
2021-12-16 00:00:59 +0000

    I broke my code...

---
+++ Submarine/hydrothermal_vents.py
@@ -0,0 +1,100 @@
+
+
+test_list_of_strings = ["0,9 -> 5,9\n",
+   "8,0 -> 0,8\n",
+   "9,4 -> 3,4\n",
+   "2,2 -> 2,1\n",
+   "7,0 -> 7,4\n",
+   "6,4 -> 2,0\n",
+   "0,9 -> 2,9\n",
+   "3,4 -> 1,4\n",
+   "0,0 -> 8,8\n",
+   "5,5 -> 8,2\n"]
+
+test_overalps = 5
+
+
+
+class Coordinate :
+    def __init__(self, x:int, y:int) -> None:
+        self.x = x
+        self.y = y
+    
+    @classmethod
+    def from_string(cls, coordinate_string):
+        list_of_xy = list(map(int, coordinate_string.split(",")))
+        return cls(list_of_xy[0],list_of_xy[1])
+    
+
+def bresenham_line_generator(pos1, pos2) :
+    x1, y1, x2, y2 = pos1.x, pos1.y, pos2.x, pos2.y
+    
+    x, y = x1, y1
+    
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    gradient = dy/float(dx)
+
+    if gradient > 1 :
+        dx, dy = dy, dx
+        x, y = y, x
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+    
+    p = 2 * dy - dx
+
+    list_of_coords = list()
+    list_of_coords.append(Coordinate(x,y))
+    
+    for k in range(dx):
+        if p > 0 :
+            y = y + 1 if y < y2 else y - 1
+            p = p + 2 * (dy - dx)
+        else :
+            p = p + 2 * dy
+
+        x = x + 1 if x < x2 else x - 1
+
+        list_of_coords.append(Coordinate(x,y))
+
+    return list_of_coords
+
+
+class Vent_Mapping :
+    def __init__(self, direction_string) -> None:
+        list_of_cordinates = list(map(lambda x : Coordinate.from_string(x), direction_string.strip().split(" -> ")))
+        self.first = self.list_of_cordinates[0]
+        self.last = self.list_of_cordinates[1]
+        pass
+    def is_horizontal_or_vertical(self) :
+        return (self.first.x == self.last.x or self.first.y == self.last.y)
+    def get_list_of_coordinates_between_points(self) :
+        bresenham_line_generator(self.first, self.last)
+        pass
+        
+
+    
+
+class Geo_Map :
+    def __init__(self, size) -> None:
+        self.size = size
+        self.geo_points = [[0] * size for y in range(size)]
+        pass
+#    def add_vent_mapping(self, vent_map) :
+#        vent_map. 
+
+
+
+def find_overlapping_vents(list_of_strings, only_horizontal_or_vertical, map_size) :
+    list_of_vent_maps = list(map(lambda x : Vent_Mapping(x), list_of_strings))
+    geo_map = Geo_Map(map_size)
+    if only_horizontal_or_vertical :
+        list_of_vent_maps = list(filter(lambda x : x.is_horizontal_or_vertical(), list_of_vent_maps))
+    for vent_map in list_of_vent_maps :
+        vent_map.get_list_of_coordinates_between_points()
+    return 0
+
+def test_find_overlapping_vents() :
+    print(f"There are {find_overlapping_vents(test_list_of_strings, True, 10)} vents.")
+
+test_find_overlapping_vents()
