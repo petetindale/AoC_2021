51a5d23370730caab46825aaafe3abd212b0f3d6
Pete Tindale <petetindale@users.noreply.github.com>
2022-01-13 21:48:17 +0000

    parse hex

--- Submarine/packet_decoder.py
+++ Submarine/packet_decoder.py
@@ -2,9 +2,20 @@ test_list_of_strings = [
 	"D2FE28\n"
 ]
 
-def decode_packet(list_of_strings:list)->int:
-	list_of_packets = list(map(lambda x:x.strip(),list_of_strings))
+def decode_hex(hexstr:str)->int:
+		return int(hexstr,16)
+				
+
+class Packet:
+	def __init__(self, packet:str)->None:
+		self.hexstr = packet
+		self.int_pk = decode_hex(self.hexstr)
+		print(self.int_pk)
+		
 	
+
+def decode_packet(list_of_strings:list)->int:
+	list_of_packets = list(map(lambda x:Packet(x.strip()),list_of_strings))
 	return 0
 	
 def test_decode_packet():
